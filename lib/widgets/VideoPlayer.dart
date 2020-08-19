bb62ecb9c0c6aede0a6316cb9644fc1b99a4a93a
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-08-18 21:07:15 -0700

    Video Pausing Feature

--- lib/widgets/VideoPlayer.dart
+++ lib/widgets/VideoPlayer.dart
@@ -9,6 +9,7 @@ class VideoWidget extends StatefulWidget {
 class _VideoPlayerState extends State<VideoWidget> {
   VideoPlayerController _controller;
   bool _isLoading = true;
+  bool _playIsVisible = false;
 
   @override
   void initState() {
@@ -27,16 +28,61 @@ class _VideoPlayerState extends State<VideoWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return !_isLoading
-        ? AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          )
-        : Center(
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.red,
+
+    //Stack for Video Player, Play Icon, and Pausing Area
+    return Stack(
+      children: [
+
+        //Adding Video Player in Background
+        !_isLoading
+            ? SizedBox(
+                height: double.infinity,
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.red,
+                ),
+              ),
+
+        //Adding Play Icon When Video is Paused
+        Center(
+          child: Visibility(
+            visible: _playIsVisible,
+            child: Icon(
+              Icons.play_arrow,
+              color: Colors.white.withOpacity(0.7),
+              size: 90,
             ),
-          );
+          ),
+        ),
+
+        //Video Area Button for Detecting When to Pause
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Opacity(
+            opacity: 0.0,
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                  if (_controller.value.isPlaying) {
+                    _playIsVisible = true;
+                    _controller.pause();
+                  } else {
+                    _playIsVisible = false;
+                    _controller.play();
+                  }
+                });
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
