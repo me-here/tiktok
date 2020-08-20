6bbaa1cb8fe3adaa0b638cf7e38f948284d87eb7
me-here <mihirthanekar@gmail.com>
2020-08-19 19:44:06 -0700

    Started search screen

--- lib/screens/SearchScreen.dart
+++ lib/screens/SearchScreen.dart
@@ -8,9 +8,57 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return buildUI(
-      frontLayer: Container(
-        // TODO: REPLACE THIS CONTAINER WITH THE SCREEN
-        color: Colors.green,
+      frontLayer: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 40,
+              margin: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.bottom,
+                      decoration: InputDecoration(
+                        fillColor: Colors.grey[300],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: 'Search',
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 8),
+                    child: Icon(Icons.photo_filter),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200,
+              color: Colors.green,
+            ),
+            Container(
+              height: 200,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 200,
+              color: Colors.pink,
+            ),
+          ],
+        ),
       ),
     );
   }
