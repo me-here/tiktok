b0a1a2d838f4f42503fbeda32be24cf0f8bc40e9
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-08-21 16:49:02 -0700

    TabView on Profile

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -13,26 +13,11 @@ class ProfileScreen extends StatefulWidget {
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends State<ProfileScreen>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    List<String> images = [
-      "https://www.bing.com/th?id=OIP.8ocMi6RRdWyTK2I7KSdDJQHaEK&w=194&h=106&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2",
-      "https://2.bp.blogspot.com/-lfwoBpXpf8w/Wk_SHcRAjEI/AAAAAAAB28g/zd-02oGRadgXaOt-o9nuS3GxK3ZFZpC3wCLcBGAs/s1600/funny-animals-298-01.jpg",
-      "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/12/squirrel-hands-up.jpeg",
-      "https://i.ytimg.com/vi/CSHbSe7iW_E/maxresdefault.jpg",
-      "https://www.bing.com/th?id=OIP.8ocMi6RRdWyTK2I7KSdDJQHaEK&w=194&h=106&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2",
-      "https://2.bp.blogspot.com/-lfwoBpXpf8w/Wk_SHcRAjEI/AAAAAAAB28g/zd-02oGRadgXaOt-o9nuS3GxK3ZFZpC3wCLcBGAs/s1600/funny-animals-298-01.jpg",
-      "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/12/squirrel-hands-up.jpeg",
-      "https://i.ytimg.com/vi/CSHbSe7iW_E/maxresdefault.jpg",
-      "https://www.bing.com/th?id=OIP.8ocMi6RRdWyTK2I7KSdDJQHaEK&w=194&h=106&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2",
-      "https://2.bp.blogspot.com/-lfwoBpXpf8w/Wk_SHcRAjEI/AAAAAAAB28g/zd-02oGRadgXaOt-o9nuS3GxK3ZFZpC3wCLcBGAs/s1600/funny-animals-298-01.jpg",
-      "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/12/squirrel-hands-up.jpeg",
-      "https://i.ytimg.com/vi/CSHbSe7iW_E/maxresdefault.jpg",
-      "https://www.bing.com/th?id=OIP.8ocMi6RRdWyTK2I7KSdDJQHaEK&w=194&h=106&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2",
-      "https://2.bp.blogspot.com/-lfwoBpXpf8w/Wk_SHcRAjEI/AAAAAAAB28g/zd-02oGRadgXaOt-o9nuS3GxK3ZFZpC3wCLcBGAs/s1600/funny-animals-298-01.jpg",
-      "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/12/squirrel-hands-up.jpeg",
-      "https://i.ytimg.com/vi/CSHbSe7iW_E/maxresdefault.jpg",
+    List<String> userVideos = [
       "https://www.bing.com/th?id=OIP.8ocMi6RRdWyTK2I7KSdDJQHaEK&w=194&h=106&c=8&rs=1&qlt=90&dpr=1.25&pid=3.1&rm=2",
       "https://2.bp.blogspot.com/-lfwoBpXpf8w/Wk_SHcRAjEI/AAAAAAAB28g/zd-02oGRadgXaOt-o9nuS3GxK3ZFZpC3wCLcBGAs/s1600/funny-animals-298-01.jpg",
       "https://allthatsinteresting.com/wordpress/wp-content/uploads/2018/12/squirrel-hands-up.jpeg",
@@ -43,8 +28,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
       "https://i.ytimg.com/vi/CSHbSe7iW_E/maxresdefault.jpg",
     ];
 
+    List<String> userLikedVideos = [];
+
+    List<String> userPrivateVideos = [];
+
     //Calculates the height of the gridview with all elements
     double gridVidAspectRatio = 27 / 35; //vidWidth/vidHeight
+
+    TabController tabControl = new TabController(vsync: this, length: 3);
 
     final profileAppBar = WhiteAppBar(
       leading: Icons.perm_identity,
@@ -54,41 +45,90 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final tabsBar = SliverAppBar(
       backgroundColor: Colors.white,
       pinned: true,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(Icons.apps),
+      title: TabBar(
+        tabs: [
+          Icon(
+            Icons.apps,
             color: Colors.black,
-            onPressed: () {},
           ),
-          IconButton(
-            icon: Icon(Icons.favorite),
+          Icon(
+            Icons.favorite,
             color: Colors.black,
-            onPressed: () {},
           ),
-          IconButton(
-            icon: Icon(Icons.lock),
+          Icon(
+            Icons.lock,
             color: Colors.black,
-            onPressed: () {},
           ),
         ],
+        indicatorColor: Colors.black,
+        indicatorSize: TabBarIndicatorSize.label,
+        controller: tabControl,
       ),
     );
 
-    final usersVideoGrid = SliverGrid.count(
-      crossAxisCount: 3,
-      crossAxisSpacing: 3,
-      mainAxisSpacing: 3,
-      childAspectRatio: gridVidAspectRatio,
-      children: List.generate(images.length, (index) {
-        return Container(
-          child: Image.network(
-            images[index],
-            fit: BoxFit.fill,
-          ),
-        );
-      }),
+    final userVideoGrid = IgnorePointer(
+      child: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 3,
+        mainAxisSpacing: 3,
+        shrinkWrap: true,
+        childAspectRatio: gridVidAspectRatio,
+        children: List.generate(userVideos.length, (index) {
+          return Container(
+            child: Image.network(
+              userVideos[index],
+              fit: BoxFit.fill,
+            ),
+          );
+        }),
+      ),
+    );
+
+    final userVideoLikedGrid = IgnorePointer(
+      child: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 3,
+        mainAxisSpacing: 3,
+        shrinkWrap: true,
+        childAspectRatio: gridVidAspectRatio,
+        children: List.generate(userLikedVideos.length, (index) {
+          return Container(
+            child: Image.network(
+              userLikedVideos[index],
+              fit: BoxFit.fill,
+            ),
+          );
+        }),
+      ),
+    );
+
+    final userVideoPrivatesGrid = IgnorePointer(
+      child: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 3,
+        mainAxisSpacing: 3,
+        shrinkWrap: true,
+        childAspectRatio: gridVidAspectRatio,
+        children: List.generate(userPrivateVideos.length, (index) {
+          return Container(
+            child: Image.network(
+              userPrivateVideos[index],
+              fit: BoxFit.fill,
+            ),
+          );
+        }),
+      ),
+    );
+
+    final gridsWithCorrespondingTabs = SliverFillRemaining(
+      child: TabBarView(
+        controller: tabControl,
+        children: <Widget>[
+          userVideoGrid,
+          userVideoLikedGrid,
+          userVideoPrivatesGrid,
+        ],
+      ),
     );
 
     return widget.buildUI(
@@ -100,7 +140,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
               slivers: <Widget>[
                 ProfileInfo(),
                 tabsBar,
-                usersVideoGrid,
+                gridsWithCorrespondingTabs,
               ],
             ),
           ),
