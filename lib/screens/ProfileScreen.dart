68687078092fe37e6afd9b74cd3718571bd208f1
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-08-20 20:28:37 -0700

    Profile Scrolling with Slivers

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok/widgets/WhiteAppBar.dart';
+import '../widgets/ProfileInfo.dart';
 
 /// This is where you see your profile.
 class ProfileScreen extends StatefulWidget {
@@ -43,131 +44,42 @@ class _ProfileScreenState extends State<ProfileScreen> {
     ];
 
     //Calculates the height of the gridview with all elements
-    double gridVidAspectRatio = 27/35; //vidWidth/vidHeight
-    double gridViewContentHeight = (MediaQuery.of(context).size.width / 3.0) *
-        (images.length / 3.0).ceil() * (1 / gridVidAspectRatio);
-
-    Widget _createStats(int statNum, String statName) {
-      return Container(
-        width: 110,
-        child: Column(
-          children: [
-            Text(
-              "$statNum",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            Text(
-              statName,
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 15,
-              ),
-            ),
-          ],
-        ),
-      );
-    }
+    double gridVidAspectRatio = 27 / 35; //vidWidth/vidHeight
 
     final profileAppBar = WhiteAppBar(
       leading: Icons.perm_identity,
       trailing: Icons.menu,
     );
 
-    final profileInfoArea = Container(
-      height: MediaQuery.of(context).size.height * .4,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Column(
-            children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "http://www.adrants.com/images/tik_tok_logo.jpeg"),
-                radius: MediaQuery.of(context).size.width * .1,
-              ),
-              SizedBox(height: 4),
-              Text(
-                "@username",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _createStats(0, "Following"),
-              _createStats(0, "Followers"),
-              _createStats(0, "Likes"),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RaisedButton(
-                color: Colors.white,
-                child: Text(
-                  "Edit profile",
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-                onPressed: () {},
-              ),
-              SizedBox(width: 6),
-              ButtonTheme(
-                minWidth: 56,
-                child: RaisedButton(
-                  color: Colors.white,
-                  child: Icon(Icons.bookmark_border),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-
-    final profileContentTabBar = Container(
-      height: MediaQuery.of(context).size.height * .07,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey,
-          width: 1,
-        ),
-      ),
-      child: Row(
+    final tabsBar = SliverAppBar(
+      backgroundColor: Colors.white,
+      pinned: true,
+      title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           IconButton(
             icon: Icon(Icons.apps),
+            color: Colors.black,
             onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.favorite),
+            color: Colors.black,
             onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.lock),
+            color: Colors.black,
             onPressed: () {},
           ),
         ],
       ),
     );
 
-    final usersVideoGrid = GridView.count(
+    final usersVideoGrid = SliverGrid.count(
       crossAxisCount: 3,
       crossAxisSpacing: 3,
       mainAxisSpacing: 3,
-      scrollDirection: Axis.vertical,
-      physics: ScrollPhysics(),
-      shrinkWrap: true,
       childAspectRatio: gridVidAspectRatio,
       children: List.generate(images.length, (index) {
         return Container(
@@ -184,18 +96,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
         children: [
           profileAppBar,
           Expanded(
-            child: SingleChildScrollView(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * .48 +
-                    gridViewContentHeight,
-                child: Column(
-                  children: [
-                    profileInfoArea,
-                    profileContentTabBar,
-                    usersVideoGrid,
-                  ],
-                ),
-              ),
+            child: CustomScrollView(
+              slivers: <Widget>[
+                ProfileInfo(),
+                tabsBar,
+                usersVideoGrid,
+              ],
             ),
           ),
         ],
---
+++ lib/widgets/ProfileInfo.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class ProfileInfo extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      delegate: ProfileInfoDelegate(),
+    );
+  }
+}
+
+class ProfileInfoDelegate extends SliverPersistentHeaderDelegate {
+
+  double _profileInfoHeight = 300;
+
+  Widget _createStats(int statNum, String statName) {
+    return Container(
+      width: 110,
+      child: Column(
+        children: [
+          Text(
+            "$statNum",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            statName,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    _profileInfoHeight = MediaQuery.of(context).size.height * .4;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "http://www.adrants.com/images/tik_tok_logo.jpeg"),
+              radius: MediaQuery.of(context).size.width * .1,
+            ),
+            SizedBox(height: 4),
+            Text(
+              "@username",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _createStats(0, "Following"),
+            _createStats(0, "Followers"),
+            _createStats(0, "Likes"),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              color: Colors.white,
+              child: Text(
+                "Edit profile",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(width: 6),
+            ButtonTheme(
+              minWidth: 56,
+              child: RaisedButton(
+                color: Colors.white,
+                child: Icon(Icons.bookmark_border),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => _profileInfoHeight;
+
+  @override
+  double get minExtent => _profileInfoHeight - 1;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
