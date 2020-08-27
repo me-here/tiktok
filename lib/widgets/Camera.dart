f8f04ddcef98e6da1cf139d9df429c457771359e
me-here <mihirthanekar@gmail.com>
2020-08-26 19:19:16 -0700

    Major refactor of camera widgets.

--- lib/widgets/Camera.dart
+++ lib/widgets/Camera.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+
+import 'package:camera/camera.dart';
 import 'package:path_provider/path_provider.dart';
 
 class CameraWidget extends StatefulWidget {
@@ -14,48 +14,7 @@ class CameraWidget extends StatefulWidget {
 class _CameraWidgetState extends State<CameraWidget> {
   List<CameraDescription> cameras;
   CameraController _controller;
-  List<IconButton> _topButtons = [
-    IconButton(
-      icon: Icon(
-        Icons.exit_to_app,
-        color: Colors.white,
-        size: 30,
-      ),
-      onPressed: () {},
-    ),
-    IconButton(
-      icon: Icon(
-        Icons.color_lens,
-        color: Colors.white,
-        size: 30,
-      ),
-      onPressed: () {},
-    ),
-    IconButton(
-      icon: Icon(
-        Icons.music_note,
-        color: Colors.white,
-        size: 30,
-      ),
-      onPressed: () {},
-    ),
-    IconButton(
-      icon: Icon(
-        Icons.timelapse,
-        color: Colors.white,
-        size: 30,
-      ),
-      onPressed: () {},
-    ),
-    IconButton(
-      icon: Icon(
-        Icons.slow_motion_video,
-        color: Colors.white,
-        size: 30,
-      ),
-      onPressed: () {},
-    ),
-  ];
+
   String videoPath;
   String timestamp() => new DateTime.now().millisecondsSinceEpoch.toString();
   bool _startRecording = false;
@@ -64,7 +23,7 @@ class _CameraWidgetState extends State<CameraWidget> {
     try {
       WidgetsFlutterBinding.ensureInitialized();
       cameras = await availableCameras();
-      _controller = CameraController(cameras[0], ResolutionPreset.high);
+      _controller = CameraController(cameras[0], ResolutionPreset.medium);
       await _controller.initialize();
       await _controller.prepareForVideoRecording();
     } on Exception catch (_) {
@@ -158,115 +117,158 @@ class _CameraWidgetState extends State<CameraWidget> {
 
   @override
   Widget build(BuildContext context) {
+    /// This is the actual preview you see of the camera.
+    final _cameraPreview = Container(
+      width: MediaQuery.of(context).size.width,
+      child: AspectRatio(
+        aspectRatio: _controller.value.aspectRatio,
+        child: CameraPreview(_controller),
+      ),
+    );
+
+    /// These are the buttons at the bottom that let you shoot/record/upload.
+    final _bottomButtons = [
+      _BottomCameraButton(
+        alignment: Alignment.bottomLeft,
+        onPressed: () {},
+        color: Colors.teal,
+        label: 'Effects',
+      ),
+      _BottomCameraButton(
+        alignment: Alignment.bottomCenter,
+        onPressed: !_startRecording ? onStartRecord : onStopRecord,
+        color: Colors.red,
+        label: 'Tap to Shoot',
+      ),
+      _BottomCameraButton(
+        alignment: Alignment.bottomRight,
+        onPressed: () {},
+        color: Colors.yellow,
+        label: 'Upload',
+      ),
+    ];
+
+    final _topBar = Container(
+      margin: EdgeInsets.symmetric(
+          vertical: MediaQuery.of(context).size.height * 0.05),
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: _TopActionBar(),
+    );
+
+    /// This is the composition of everything that should be displayed in this screen.
+    final _cameraScreen = Stack(
+      children: [
+        _cameraPreview,
+        ..._bottomButtons,
+        _topBar,
+      ],
+    );
+
     return Scaffold(
       body: _controller == null
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: CameraPreview(_controller),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: RawMaterialButton(
-                          fillColor: Colors.teal,
-                          padding: EdgeInsets.all(20),
-                          shape: CircleBorder(),
-                          onPressed: () {},
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'Effects',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: RawMaterialButton(
-                          fillColor: Colors.red,
-                          padding: EdgeInsets.all(35),
-                          shape: CircleBorder(),
-                          onPressed:
-                              !_startRecording ? onStartRecord : onStopRecord,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'Tap to Shoot',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: RawMaterialButton(
-                          fillColor: Colors.yellow,
-                          padding: EdgeInsets.all(20),
-                          shape: CircleBorder(),
-                          onPressed: () {},
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          'Upload',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                      vertical: MediaQuery.of(context).size.height * 0.05),
-                  height: MediaQuery.of(context).size.height * 0.1,
-                  child: ListView.builder(
-                      physics: ClampingScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                      itemCount: _topButtons.length,
-                      itemBuilder: (BuildContext ctx, int index) {
-                        return Container(
-                          margin: EdgeInsets.all(20),
-                          child: _topButtons[index],
-                        );
-                      }),
-                ),
-              ],
-            ),
+          : _cameraScreen,
     );
   }
 
   void dispose() {
     super.dispose();
     _controller.dispose();
+  }
+}
+
+/// This is the circular button that lets you record and upload, etc.
+class _BottomCameraButton extends StatelessWidget {
+  final Alignment alignment;
+  final Function onPressed;
+  final Color color;
+  final String label;
+
+  const _BottomCameraButton({
+    @required this.alignment,
+    @required this.onPressed,
+    @required this.color,
+    @required this.label,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: RawMaterialButton(
+              fillColor: color,
+              padding: alignment == Alignment.bottomCenter
+                  ? EdgeInsets.all(35)
+                  : EdgeInsets.all(20),
+              shape: CircleBorder(),
+              onPressed: onPressed,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Text(
+              label,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// This is the combination of all the TopButtons, forming the TopActionBar
+class _TopActionBar extends StatelessWidget {
+  const _TopActionBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        _TopButton(icon: Icons.exit_to_app, onPressed: () => {}),
+        _TopButton(icon: Icons.color_lens, onPressed: () => {}),
+        _TopButton(icon: Icons.music_note, onPressed: () => {}),
+        _TopButton(icon: Icons.timelapse, onPressed: () => {}),
+        _TopButton(icon: Icons.slow_motion_video, onPressed: () => {}),
+      ],
+      scrollDirection: Axis.horizontal,
+    );
+  }
+}
+
+/// This widget is the top icon button in the horizontal listview.
+class _TopButton extends StatelessWidget {
+  const _TopButton({
+    Key key,
+    @required this.icon,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: IconButton(
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 30,
+        ),
+        onPressed: onPressed,
+      ),
+    );
   }
 }
