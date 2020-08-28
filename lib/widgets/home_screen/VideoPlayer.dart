737a3e217a6e1968c47573e92b4926c74be633c2
me-here <mihirthanekar@gmail.com>
2020-08-28 15:51:42 -0700

    refactor VideoPlayer.dart

--- lib/widgets/home_screen/VideoPlayer.dart
+++ lib/widgets/home_screen/VideoPlayer.dart
@@ -5,7 +5,6 @@ import 'package:video_player/video_player.dart';
 import 'package:tiktok/providers/VideoStatus.dart';
 
 class VideoWidget extends StatefulWidget {
-
   final String videoLink;
 
   VideoWidget(this.videoLink);
@@ -22,19 +21,35 @@ class _VideoPlayerState extends State<VideoWidget> {
   @override
   void initState() {
     super.initState();
+    initializeController();
+  }
+
+  Future<void> initializeController() async {
     _controller = VideoPlayerController.network(
       widget.videoLink,
-    )..initialize().then((_) {
-        print('initializing');
-        setState(() {
-          _controller.play();
-          _isLoading = false;
-          _controller.setLooping(true);
+    );
+    await _controller.initialize();
 
-          //Sets the video status to isPlaying true for disc rotation animation
-          Provider.of<VideoStatus>(context, listen: false).setVidStatus(_controller.value.isPlaying);
-        });
-      });
+    setState(() {
+      _isLoading = false;
+      _controller.play();
+      _controller.setLooping(true);
+    });
+
+    //Sets the video status to isPlaying true for disc rotation animation
+    Provider.of<VideoStatus>(context, listen: false)
+        .setVidStatus(_controller.value.isPlaying);
+  }
+
+  void togglePause() {
+    setState(() {
+      _playIsVisible = !_playIsVisible;
+      _controller.value.isPlaying ? _controller.pause() : _controller.play();
+
+      //Listens for a change in video playing and sends it to ShareIcons.dart for the rotating disc animation
+      Provider.of<VideoStatus>(context, listen: false)
+          .setVidStatus(_controller.value.isPlaying);
+    });
   }
 
   @override
@@ -44,13 +59,7 @@ class _VideoPlayerState extends State<VideoWidget> {
       children: [
         //Adding Video Player in Background
         !_isLoading
-            ? SizedBox(
-                height: double.infinity,
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                ),
-              )
+            ? VideoPlayer(_controller)
             : Center(
                 child: CircularProgressIndicator(
                   backgroundColor: Colors.red,
@@ -59,38 +68,19 @@ class _VideoPlayerState extends State<VideoWidget> {
 
         //Adding Play Icon When Video is Paused
         Center(
-          child: Visibility(
-            visible: _playIsVisible,
-            child: Icon(
-              Icons.play_arrow,
-              color: Colors.white.withOpacity(0.7),
-              size: 90,
-            ),
-          ),
+          child: _playIsVisible
+              ? Icon(
+                  Icons.play_arrow,
+                  color: Colors.white.withOpacity(0.7),
+                  size: 90,
+                )
+              : null,
         ),
 
         //Video Area Button for Detecting When to Pause
-        SizedBox(
-          height: double.infinity,
-          width: double.infinity,
-          child: Opacity(
-            opacity: 0.0,
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  if (_controller.value.isPlaying) {
-                    _playIsVisible = true;
-                    _controller.pause();
-                  } else {
-                    _playIsVisible = false;
-                    _controller.play();
-                  }
-                  //Listens for a change in video playing and sends it to ShareIcons.dart for the rotating disc animation
-                  Provider.of<VideoStatus>(context, listen: false).setVidStatus(_controller.value.isPlaying);
-                });
-              },
-            ),
-          ),
+        GestureDetector(
+          onTap: togglePause,
+          behavior: HitTestBehavior.translucent,
         ),
       ],
     );
