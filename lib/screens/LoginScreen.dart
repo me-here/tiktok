c36121e29ca338789caff664d9c532c73978fe97
me-here <mihirthanekar@gmail.com>
2020-08-27 17:03:31 -0700

    NEW SHA CERTIFICATE FIXES AUTH

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_signin_button/button_builder.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,30 +8,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: [
-      'email',
-      //'https://www.googleapis.com/auth/contacts.readonly',
-    ],
-  );
+  GoogleSignInAccount _currentUser;
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+      setState(() {
+        _currentUser = account;
+      });
+      print(_currentUser);
+    });
+    _googleSignIn.signInSilently();
+  }
 
   Future<void> _handleSignIn() async {
     try {
       await _googleSignIn.signIn();
     } catch (error) {
+      print('ERRR IN SIGN IN');
       print(error);
     }
-  }
-
-  @override
-  void initState() {
-    initializeAuth();
-    super.initState();
-  }
-
-  void initializeAuth() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
   }
 
   @override
@@ -47,11 +44,9 @@ class _LoginScreenState extends State<LoginScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text('Login by pressing the button below.'),
-              SignInButtonBuilder(
-                backgroundColor: Colors.blue,
+              SignInButton(
+                Buttons.Google,
                 onPressed: () => _handleSignIn(),
-                text: 'Sign in with Google',
-                icon: Icons.ac_unit,
               )
             ],
           ),
